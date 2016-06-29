63903878de157305f9fbcf3bd2db3e7f7a52a2eb
Hannes Weisbach <hannes.weisbach@tu-dresden.de>
2016-06-29 18:03:56 +0200

    Add Load balancing tracepoints

--- instrumentation/events/lttng-module/atlas.h
+++ instrumentation/events/lttng-module/atlas.h
@@ -7,6 +7,7 @@
 #ifndef __LTTNG_ATLAS_HAVE_SCHED_H
 #define __LTTNG_ATLAS_HAVE_SCHED_H
 #include "sched/sched.h"
+#include <asm/tsc.h>
 #endif
 
 #include "../../../probes/lttng-tracepoint-event.h"
@@ -48,7 +49,9 @@ LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_job_template, atlas_job_migrated,
 
 LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_job_template, atlas_job_start,
                                 TP_PROTO(struct atlas_job *j), TP_ARGS(j))
-LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_job_template, atlas_job_missed,
+LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_job_template, atlas_job_soft_miss,
+                                TP_PROTO(struct atlas_job *j), TP_ARGS(j))
+LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_job_template, atlas_job_hard_miss,
                                 TP_PROTO(struct atlas_job *j), TP_ARGS(j))
 LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_job_template, atlas_job_select,
                                 TP_PROTO(struct atlas_job *j), TP_ARGS(j))
@@ -71,6 +74,71 @@ LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_task_template, atlas_task_sleep,
                                 TP_PROTO(struct task_struct *p), TP_ARGS(p))
 LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_task_template, atlas_task_wakeup,
                                 TP_PROTO(struct task_struct *p), TP_ARGS(p))
+
+LTTNG_TRACEPOINT_EVENT_CLASS(atlas_task_migrate_template,
+	TP_PROTO(struct task_struct * p, int other_cpu),
+	TP_ARGS(p, other_cpu),
+	TP_FIELDS(
+		ctf_array_text(	char,	comm,		p->comm, TASK_COMM_LEN)
+		ctf_integer(	pid_t,	tid,		task_pid_nr_ns(p, task_active_pid_ns(p)))
+		ctf_integer(	int,	task_policy,	p->policy)
+		ctf_integer(	int,	other_cpu,	other_cpu)
+	)
+)
+
+LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_task_migrate_template, atlas_task_migrate,
+                                TP_PROTO(struct task_struct *p, int other_cpu),
+                                TP_ARGS(p, other_cpu))
+LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_task_migrate_template,
+                                atlas_task_overload_pulled,
+                                TP_PROTO(struct task_struct *p, int other_cpu),
+                                TP_ARGS(p, other_cpu))
+LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_task_migrate_template,
+                                atlas_task_idle_balanced,
+                                TP_PROTO(struct task_struct *p, int other_cpu),
+                                TP_ARGS(p, other_cpu))
+
+LTTNG_TRACEPOINT_EVENT_CLASS(atlas_probe_template,
+	TP_PROTO(void * dummy),
+	TP_ARGS(dummy),
+	TP_FIELDS(
+		ctf_integer(s64, now, ktime_to_ns(ktime_get()))
+		ctf_integer(u64, cycles, get_cycles())
+	)
+)
+
+LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_probe_template, atlas_probe_detach,
+                                TP_PROTO(void *p), TP_ARGS(p))
+LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_probe_template, atlas_probe_detached,
+                                TP_PROTO(void *p), TP_ARGS(p))
+LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_probe_template, atlas_probe_attach,
+                                TP_PROTO(void *p), TP_ARGS(p))
+LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_probe_template, atlas_probe_attached,
+                                TP_PROTO(void *p), TP_ARGS(p))
+
+LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_probe_template,
+                                atlas_probe_overload_notify, TP_PROTO(void *p),
+                                TP_ARGS(p))
+LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_probe_template,
+                                atlas_probe_overload_notified,
+                                TP_PROTO(void *p), TP_ARGS(p))
+
+LTTNG_TRACEPOINT_EVENT_CLASS(atlas_ipi_template,
+	TP_PROTO(int cpu),
+	TP_ARGS(cpu),
+	TP_FIELDS(
+		ctf_integer(s64, now, ktime_to_ns(ktime_get()))
+		ctf_integer(u64, cycles, get_cycles())
+		ctf_integer(int, cpu, cpu)
+	)
+)
+
+LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_ipi_template, atlas_ipi_send,
+                                TP_PROTO(int cpu), TP_ARGS(cpu))
+LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_ipi_template, atlas_ipi_recv,
+                                TP_PROTO(int cpu), TP_ARGS(cpu))
+LTTNG_TRACEPOINT_EVENT_INSTANCE(atlas_ipi_template, atlas_ipi_handle,
+                                TP_PROTO(int cpu), TP_ARGS(cpu))
 
 #endif /* LTTNG_TRACE_ATLAS_H */
 
